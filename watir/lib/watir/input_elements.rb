5ce0112821c2116a64e27363e19a3a5a44a296b5
Bret Pettichord <bret@pettichord.com>
2010-07-13 14:31:33 -0500

    Rework code for IE#file_field so that it is faster and will now report errors when it fails. Paired with Hugh McGowan.

--- watir/lib/watir/input_elements.rb
+++ watir/lib/watir/input_elements.rb
@@ -445,34 +445,25 @@ module Watir
     # set the file location in the Choose file dialog in a new process
     # will raise a Watir Exception if AutoIt is not correctly installed
     def set(path_to_file)
-      assert_exists
       require 'watir/windowhelper'
       WindowHelper.check_autoit_installed
-      begin
-        Thread.new do
-          sleep 1 # it takes some time for popup to appear
+      assert_exists
+      click_no_wait
 
-          system %{ruby -e '
-              require "win32ole"
-
-              @autoit = WIN32OLE.new("AutoItX3.Control")
-              time    = Time.now
-
-              while (Time.now - time) < 15 # the loop will wait up to 15 seconds for popup to appear
-                #{POPUP_TITLES.inspect}.each do |popup_title|
-                  next unless @autoit.WinWait(popup_title, "", 1) == 1
-
-                  @autoit.ControlSetText(popup_title, "", "Edit1", #{path_to_file.inspect})
-                  @autoit.ControlSend(popup_title, "", "Button2", "{ENTER}")
-                  exit
-                end # each
-              end # while
-          '}
-        end.join(1)
-      rescue
-        raise Watir::Exception::WatirException, "Problem accessing Choose file dialog"
+      autoit = WIN32OLE.new("AutoItX3.Control")
+      require 'timeout'
+      Timeout::timeout(15) do
+        while true
+          POPUP_TITLES.each do |popup_title|
+            next unless autoit.WinWait(popup_title, "", 1) == 1
+            autoit.ControlSetText(popup_title, "", "Edit1", path_to_file)
+            autoit.ControlSend(popup_title, "", "Button2", "{ENTER}")
+            return
+          end # each
+          sleep 0.05
+        end # while
       end
-      click
+
     end
   end
   
